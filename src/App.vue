3ea4db474bfaa9e3c1db5b12e908f481762829fa
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-10 14:40:01 +0100

    Imported store.js and axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue';
 
+import axios from 'axios';
+import { store } from './store.js'
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      store
+    }
+  }
 }
 </script>
 
 <template lang="">
   <div>
-    
+    <AppHeader />
   </div>
 </template>
 
-<style lang="">
-  
+<style lang="scss">
+@use './styles/generals.scss'
 </style>
