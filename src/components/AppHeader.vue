5553047a92400972aaff2ccc53cee4554e837c0f
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-10 14:44:03 +0100

    [UPDATE] Added Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template lang="">
+    <header>
+        <div class="container-fluid">
+            <div class="d-flex my-3">
+                <img src="../assets/Yugioh_anime_logo.webp" alt="" class="img-fluid ms-2">
+                <h1 class="ms-5">Yu-Gi-Oh Api</h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    img {
+        width: 100px;
+    }
+}
+</style>
